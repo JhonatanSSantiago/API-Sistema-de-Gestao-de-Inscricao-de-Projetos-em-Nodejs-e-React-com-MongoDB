74bcafcdd13e1e9997c5e9df12bbe80cd727618f
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-03-17 19:53:58 -0300

    criando model Avaliador

---
+++ backend/model/Avaliador.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const avaliadorSchema = new Schema(
+    {
+        nome: {
+            type: String,
+            required: true
+        },
+        cpf: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        telefone: {
+            type: String,
+            required: true
+        },
+        endereco: {
+            type: String,
+            required: true
+        },
+        formacao: {
+            type: String,
+            required: true
+        },
+    },{ timestamps: true} //salva data de criação e atualização
+);
+
+const Avaliador = mongoose.model("Avaliador", avaliadorSchema);
+
+module.exports = {
+    Avaliador,
+    autoravaliadorSchemaSchema,
+};
