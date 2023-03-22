22ba709b89668bcd8720896618dbb434c532f04f
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-03-22 19:15:08 -0300

    Atualizando console.log

--- backend/controllers/autorController.js
+++ backend/controllers/autorController.js
@@ -4,7 +4,6 @@ const autorController = {
 
     create: async(req, res) => {
         try {
-
             const autor = {
                 nome: req.body.nome,
                 cpf: req.body.cpf,
@@ -13,57 +12,45 @@ const autorController = {
                 endereco: req.body.endereco,
                 formacao: req.body.formacao
             };
-
             const response = await AutorModel.create(autor);
-
             res.status(201).json({response, msg: "Autor criado com sucesso!"});
-
         } catch (error){
-            console.log(error);
+            console.log("Erro: "+ error);
         }
     },
     getAll: async(req, res) => {
         try {
             const autor = await AutorModel.find();
-            res.json(autor);
-            
+            res.json(autor);           
         } catch (error) {
-            console.log(error);
+            console.log("Erro: "+ error);
         }
     },
     get: async(req, res) => {
         try {
             const id = req.params.id;
             const autor = await AutorModel.findById(id);
-
             if(!autor){
                 res.status(404).json({msg: "Autor não encontrado"});
                 return;
             }
-
             res.json(autor);
         } catch (error) {
-            console.log(error);
+            console.log("Erro: "+ error);
         }
     },
     delete: async(req, res) => {
         try {
-
             const id = req.params.id
             const autor = await AutorModel.findById(id);
-
             if(!autor){
                 res.status(404).json({msg: "Autor não encontrado"});
                 return;
             }
-
             const deletedAutor = await AutorModel.findByIdAndDelete(id);
-
-            res.status(200).json({deletedAutor, msg: "Autor excluído com sucesso"});
-
-            
+            res.status(200).json({deletedAutor, msg: "Autor excluído com sucesso"});            
         } catch (error) {
-            console.log(error);
+            console.log("Erro: "+ error);
         }
     }
     
--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 async function main(){
     try {
         mongoose.set("strictQuery", true);
-        await mongoose.connect('mongodb+srv://admin:ro37MpIbidRAxqVO@sgip.osttyfg.mongodb.net/?retryWrites=true&w=majority');
+      //  await mongoose.connect('mongodb+srv://admin:ro37MpIbidRAxqVO@sgip.osttyfg.mongodb.net/?retryWrites=true&w=majority');
+         await mongoose.connect('mongodb://127.0.0.1:27017/sgip');
         console.log("Conectado do Banco!");
     } catch (error) {
-        console.log("Erro: ${eror}");
+        console.log("Erro: "+ error);
     }
 }
 
