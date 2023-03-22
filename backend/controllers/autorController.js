6a81741369289cb161ac72dd5e7ae8afa47059aa
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-03-22 09:21:51 -0300

    Criando create autor

--- backend/controllers/autorController.js
+++ backend/controllers/autorController.js
@@ -2,6 +2,26 @@ const { Autor: AutorModel} = require("../model/Autor");
 
 const autorController = {
 
+    create: async(req, res) => {
+        try {
+
+            const autor = {
+                nome: req.body.nome,
+                cpf: req.body.cpf,
+                email: req.body.email,
+                telefone: req.body.telefone,
+                endereco: req.body.endereco,
+                formacao: req.body.formacao
+            };
+
+            const response = await AutorModel.create(autor);
+
+            res.status(201).json({response, msg: "Autor criado com sucesso!"});
+
+        } catch (error){
+            console.log(error);
+        }
+    }
     
 }
 
--- backend/routes/autorRoute.js
+++ backend/routes/autorRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const autorController = require("../controllers/autorController");
 
+router.route("/autor").post((req, res) => autorController.create(req, res));
 
 module.exports = router;
