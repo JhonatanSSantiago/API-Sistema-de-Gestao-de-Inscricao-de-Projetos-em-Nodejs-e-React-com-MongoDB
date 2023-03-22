79ef1d2797de9e4b4dd2ae2c77183d2f6149b05c
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-03-22 08:31:11 -0300

    mudança de banco de dados

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function main(){
     try {
         mongoose.set("strictQuery", true);
-        await mongoose.connect('mongodb://127.0.0.1:27017/sgip');
+        await mongoose.connect('mongodb+srv://admin:ro37MpIbidRAxqVO@sgip.osttyfg.mongodb.net/?retryWrites=true&w=majority');
         console.log("Conectado do Banco!");
     } catch (error) {
         console.log("Erro: ${eror}");
