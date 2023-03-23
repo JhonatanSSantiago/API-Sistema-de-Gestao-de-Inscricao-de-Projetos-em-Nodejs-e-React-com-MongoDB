906cf8f9fe0399d424768e0e1938bb1cff275b8d
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-03-23 15:50:32 -0300

    Criando create Projeto

--- backend/controllers/projetoController.js
+++ backend/controllers/projetoController.js
@@ -1,7 +1,22 @@
 const { Projeto: ProjetoModel } = require("../model/Projeto");
 
 const projetoController = {
-
+    create: async(req, res) => {
+        try{
+            const projeto = {
+                area: req.body.area,
+                titulo: req.body.titulo,
+                resumo: req.body.resumo,
+                autor: req.body.autor,
+                status: req.body.status,
+                data_envio: req.body.data_envio,
+            };
+            const response = await ProjetoModel.create(projeto);
+            res.status(201).json({response, msg: "Projeto Salvo com sucesso"})
+        } catch (error){
+            console.log("Erro: "+ error);
+        }
+    }
 };
 
 module.exports = projetoController;
--- backend/routes/projetoRoute.js
+++ backend/routes/projetoRoute.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 
 const projetoController = require("../controllers/projetoController");
 
+router.route("/projeto").post((req, res)=> projetoController.create(req, res));
+
+
 
 module.exports = router;
