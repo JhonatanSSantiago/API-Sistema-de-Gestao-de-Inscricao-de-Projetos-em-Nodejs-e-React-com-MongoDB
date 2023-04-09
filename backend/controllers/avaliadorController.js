58cd3a3cddadc055f6176c98ac403a07a5f2449b
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-04-09 10:06:16 -0300

    Login Avaliador

--- backend/controllers/avaliadorController.js
+++ backend/controllers/avaliadorController.js
@@ -1,16 +1,43 @@
 const { Avaliador: AvaliadorModel} = require("../model/Avaliador");
+const bcrypt = require('bcrypt');
 
 const avaliadorController = {
 
     create: async(req, res) => {
+        const {nome, cpf, email, senha, confirmedSenha, telefone, endereco, formacao} = req.body
+
+        //validações
+        if(!nome) {
+            return res.status(422).json({msg: "Nome obrigatorio"})
+        }
+        if(!email) {
+            return res.status(422).json({msg: "Email obrigatorio"})
+        }
+        if(!senha) {
+            return res.status(422).json({msg: "Senha obrigatoria"})
+        }
+        if(senha !== confirmedSenha) {
+            return res.status(422).json({msg: "Senhas diferentes"})
+        }
+        const avaliadorExists = await AvaliadorModel.findOne({email: email})
+
+        if(avaliadorExists){
+            return res.status(422).json({msg: "email já cadastrado"})
+        }
+
+        //cript senha
+        const salt = await bcrypt.genSalt(2)
+        const senhaHash = await bcrypt.hash(senha, salt)
+
         try {
             const avaliador = {
-                nome: req.body.nome,
-                cpf: req.body.cpf,
-                email: req.body.email,
-                telefone: req.body.telefone,
-                endereco: req.body.endereco,
-                formacao: req.body.formacao
+                nome: nome,
+                cpf: cpf,
+                email: email,
+                senha: senhaHash,
+                telefone: telefone,
+                endereco: endereco,
+                formacao: formacao
             };
             const response = await AvaliadorModel.create(avaliador);
             res.status(201).json({response, msg: "Avaliador criado com sucesso!"});
@@ -75,6 +102,40 @@ const avaliadorController = {
         }catch (error) {
             console.log("Erro: "+ error);
         }
+    },
+    login: async(req, res) => {
+        const {email, senha} = req.body
+
+        if(!email) {
+            return res.status(422).json({msg: "Email obrigatorio"})
+        }
+        if(!senha) {
+            return res.status(422).json({msg: "Senha obrigatorio"})
+        }
+
+        const avaliador = await AvaliadorModel.findOne({email: email})
+
+        if(!avaliador){
+            return res.status(404).json({msg: "email não cadastrado"})
+        }
+
+        const checkSenha = await bcrypt.compare(senha,avaliador.senha )
+
+        if(!checkSenha){
+            return res.status(422).json({msg: "senha incorreta"})
+        }
+
+        try {
+        // const secret = process.env.SECRET
+
+        //   const token = jwt.sign({id: autor._id}, secret)
+
+            res.status(200).json({idAvaliado: avaliador._id, msg: "login com sucesso"})
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({msg: error})
+        }
+
     }
     
 };
--- backend/model/Avaliador.js
+++ backend/model/Avaliador.js
@@ -15,6 +15,10 @@ const avaliadorSchema = new Schema(
             type: String,
             required: true
         },
+        senha: {
+            type: String,
+            required: true
+        },
         telefone: {
             type: String,
             required: true
