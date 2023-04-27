26d8247a2305c11b4ce09de7f1504c566aadd374
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-04-27 19:24:43 -0300

    conexão banco web

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 async function main(){
     try {
         mongoose.set("strictQuery", true);
-      //  await mongoose.connect('mongodb+srv://admin:ro37MpIbidRAxqVO@sgip.osttyfg.mongodb.net/?retryWrites=true&w=majority');
-         await mongoose.connect('mongodb://127.0.0.1:27017/sgip');
+        await mongoose.connect('mongodb+srv://admin:ro37MpIbidRAxqVO@sgip.osttyfg.mongodb.net/?retryWrites=true&w=majority');
+       //  await mongoose.connect('mongodb://127.0.0.1:27017/sgip');
         console.log("Conectado do Banco!");
     } catch (error) {
         console.log("Erro: "+ error);
