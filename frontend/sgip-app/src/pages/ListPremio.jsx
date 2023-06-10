18f8357a74dc22b0c024eec3d7b816665013ed50
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-06-10 11:26:56 -0300

    Listando Premios

--- frontend/sgip-app/src/pages/ListPremio.jsx
+++ frontend/sgip-app/src/pages/ListPremio.jsx
@@ -16,6 +16,8 @@ const ListPremio = () => {
         const data = reponse.data;
 
         console.log(data);
+        setPremios(data);
+
       } catch (error) {
         console.log(error)
       }
@@ -29,8 +31,111 @@ const ListPremio = () => {
    }, [])
 
   return (
-    <div>ListPremio</div>
-  )
-}
+    <div>
+      <h1>Premios</h1>
+      <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Descricao</th>
+                <th>Ano</th>
+                <th>Inicio</th>
+                <th>Encerramento</th>
+                <th>Período de Inscrição</th>
+                <th>Período de Avaliação</th>
+                <th>Resultado</th>
+                <th>Opção</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.nome}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.descricao}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.ano}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.inicio}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.encerramento}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.inscricao}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.avaliacao}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                        <td>{premio.resultado}</td>                                                           
+                    </div>
+                  ))
+                )}
+                </td>
+                <td>
+                {premios.length === 0 ? <p>Carregando..</p> :  (
+                  premios.map((premio) => ( 
+                    <div className="premios" key={premio._id}>                
+                       <Link to={`/listpremio/${premio._id}`} className="btn">Ver</Link>                                                          
+                    </div>
+                  ))
+                )}
+                </td>
+              </tr>
+
+             </tbody>  
+      </table>  
+    </div>
+  );
+};
 
 export default ListPremio
