01ac3e4555fdb85722e293c7b7bb911b75c2cd03
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-06-10 09:51:53 -0300

    Style Navbar

--- frontend/sgip-app/src/components/Navbar.jsx
+++ frontend/sgip-app/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
         <h2>
-            <Link to={'/'}>Blog</Link>
+            <Link to={'/'}>SGIP</Link>
         </h2>
         <ul>
             <li><Link to={'/'}>Home</Link></li>
-            <li><Link to={'/newpremio'} className="new-btn">Cadastrar Premio</Link></li>
-            <li><Link to={'/listpremio'} className="new-btn">Listar Premios</Link></li>
+            <li><Link to={'/newpremio'}>Cadastrar Premio</Link></li>
+            <li><Link to={'/listpremio'} >Listar Premios</Link></li>
         </ul>
     </nav>
   )
