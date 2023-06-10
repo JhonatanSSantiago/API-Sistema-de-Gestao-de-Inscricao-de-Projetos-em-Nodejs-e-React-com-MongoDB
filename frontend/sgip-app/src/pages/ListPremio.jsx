bda7df5c3176f8c31b435e3a002acdcb3eba34ef
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-06-10 10:26:00 -0300

    buscando dados dos premiso da API

--- frontend/sgip-app/src/pages/ListPremio.jsx
+++ frontend/sgip-app/src/pages/ListPremio.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+
+
 
 const ListPremio = () => {
+
+  const[premios, setPremios] = useState([]);
+   const getPremios = async() => {
+      
+      try {
+        
+        const reponse = await axios.get("http://localhost:3000/api/premio");
+        const data = reponse.data;
+
+        console.log(data);
+      } catch (error) {
+        console.log(error)
+      }
+
+
+
+   }
+
+   useEffect(() => {
+      getPremios()
+   }, [])
+
   return (
     <div>ListPremio</div>
   )
