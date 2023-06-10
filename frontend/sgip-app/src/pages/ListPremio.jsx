9454fad425c2cc3c856238b7f078790793c3961d
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-06-10 08:51:48 -0300

    criando componentes pages com rafec

---
+++ frontend/sgip-app/src/pages/ListPremio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListPremio = () => {
+  return (
+    <div>ListPremio</div>
+  )
+}
+
+export default ListPremio
---
+++ frontend/sgip-app/src/pages/NewPremio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPremio = () => {
+  return (
+    <div>NewPremio</div>
+  )
+}
+
+export default NewPremio;
